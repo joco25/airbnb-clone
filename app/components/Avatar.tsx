cd5ab36c4ed3520feb1c7aec475cf3cc545e4da0
jocosge <onungwej@gmail.com>
2023-11-15 08:37:03 -0500

    added authentication and location functionality

---
+++ app/components/Avatar.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import Image from 'next/image';
+import React from 'react';
+
+interface AvatarProps {
+  src?: string | null | undefined;
+}
+
+export const Avatar: React.FC<AvatarProps> = ({ src }) => {
+  return (
+    <Image
+      className='rounded-full'
+      height='30'
+      width='30'
+      src={src || '/images/placeholder.jpg'}
+      alt='Avatar'
+    />
+  );
+};
