91386cf4d330e5f6969ab29944114d664b75f792
jocosge <onungwej@gmail.com>
2023-11-15 09:05:28 -0500

    adding cloudinary

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -11,6 +11,7 @@ import { on } from 'events';
 import CountrySelect from '../inputs/CountrySelect';
 import dynamic from 'next/dynamic';
 import Counter from '../inputs/Counter';
+import ImageUpload from '../inputs/ImageUpload';
 
 enum STEPS {
   CATEGORY = 0,
@@ -149,6 +150,18 @@ const RentModal = () => {
           value={bathroomCount}
           onChange={(value) => setCustomValue('bathroomCount', value)}
         />
+      </div>
+    );
+  }
+
+  if (step === STEPS.IMAGES) {
+    bodyContent = (
+      <div className='flex flex-col gap-8'>
+        <Heading
+          title='Add a photo of your place'
+          subtitle='Show guests what your place looks like!'
+        />
+        <ImageUpload />
       </div>
     );
   }
